a0b78f9a8c6544b134119281e7f5a12011dec926
Florian Pfeffer <florian.pfeffer@outlook.com>
2021-04-10 21:34:27 +0200

    fix: cli - typos in demandCommand message

--- packages/ui5-nwabap-deployer-cli/CHANGELOG.md
+++ packages/ui5-nwabap-deployer-cli/CHANGELOG.md
@@ -1,3 +1,8 @@
+## 1.0.1 (2021-04-10)
+
+### Fixes
+- Correction of typos in demandCommand message.
+
 ## 1.0.0 (2021-04-10)
 
 ### General
--- packages/ui5-nwabap-deployer-cli/bin/cli.js
+++ packages/ui5-nwabap-deployer-cli/bin/cli.js
@@ -6,7 +6,7 @@ const deployCommand = require("../lib/commands/deploy");
 yargs.usage("\nUI5 Deployer to deploy UI5 sources to a SAP ABAP system.")
     .scriptName(scriptName)
     .command(deployCommand)
-    .demandCommand(1, "Command required. Please hava look at the help documentation above.")
+    .demandCommand(1, "Command required. Please have a look at the help documentation above.")
     .strictCommands(true)
     .strictOptions(true)
     .help(true)
